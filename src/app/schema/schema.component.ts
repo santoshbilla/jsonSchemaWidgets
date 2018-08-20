27f6f15fd4d8e9932635a6ee5186c241c01bf2ec
Radhika <rdkagnihotri@gmail.com>
2018-08-20 16:04:42 +0530

    select control list with angular schema form anf JSON Schema v4

--- src/app/schema/schema.component.ts
+++ src/app/schema/schema.component.ts
@@ -18,13 +18,43 @@ yourJsonSchema : any;
         "properties": {
           "first_name": { "type": "string" },
           "last_name": { "type": "string" },
-         "clickhere": {"type": "submit", "style":"btn-success", "title":"click here"}
-
+          "phone":{"type": "number"},
+          "email": {
+            "type": "string",
+            "pattern": "^\\S+@\\S+$",
+            "description": "Email will be used for evil."
+          },
+          "language":{
+            "type":"string",
+            "description":"this is done using Angular Schema Forms"
+          },
+          "language1": {
+            "description": "this is done using JSON Schema v4+",
+            "type": "string",
+            "oneOf":  [
+              { "title": "MongoDB",    "enum": [ "MongoDB" ] },
+              { "title": "ExpressJS",  "enum": [ "ExpressJS" ] },
+              { "title": "Angular",    "enum": [ "Angular"  ] },
+              { "title": "NodeJs",     "enum": [ "NodeJs"  ] }
+            ]
+          },
         },
-        "required": [ "last_name" ],
+        "required": [ "last_name" ,"phone", "email"],
       },
     "layout":  [
         { "type": "flex", "flex-flow": "row wrap", "items": [ "first_name", "last_name" ] },
+        {"key": "phone", "title":"phone number", "placeholder":"Enter number here"},
+        {"key":"email", "title":"Email Id", "placeholder":"Enter Email ID Here"},
+        {
+          "key":"language","titleMap": 
+          [
+            { "name": "MongoDB","value": "MongoDB" },
+            { "name": "ExpressJS","value": "ExpressJS" },
+            { "name": "Angular","value": "Angular"  },
+            { "name": "NodeJs", "value": "NodeJs"  }
+          ]
+      },
+     "language1"
       ],
     };
  }
